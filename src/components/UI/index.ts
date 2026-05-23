8a029ae011ca604a9c0c415921db48cb885858d2
Andrew Kononenko <wastardy.k@gmail.com>
2026-05-23 18:18:00 +0200

    refactor(components): move shared MiniMao and SectionLabel components from shared folder to UI for better organization

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,7 @@
 export { Dropdown } from "./Dropdown";
 export { ExportMenu } from "./ExportMenu";
 export type { ExportMenuProps } from "./ExportMenu";
+export type { TMiniMapLocation, TMiniMapProps } from "./MiniMap";
 export { PageWrapper } from "./PageWrapper";
 export { RangeSlider } from "./RangeSlider";
+export { SectionLabel } from "./SectionLabel";
