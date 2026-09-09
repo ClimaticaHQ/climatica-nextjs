97d8fa581a53a906a4e02b939eac09bba98fe185
Andrew Kononenko <wastardy.k@gmail.com>
2026-09-09 15:07:44 +0200

    feat(localization): add localization for it, fr, pt, de, el languages

--- src/constants/language.constant.ts
+++ src/constants/language.constant.ts
@@ -1,10 +1,15 @@
 export const LANGUAGES = [
+  { code: "de", label: "DE 🇩🇪" },
+  { code: "el", label: "EL 🇬🇷" },
   { code: "en", label: "EN 🇬🇧" },
   { code: "es", label: "ES 🇪🇸" },
+  { code: "fr", label: "FR 🇫🇷" },
+  { code: "it", label: "IT 🇮🇹" },
+  { code: "pt", label: "PT 🇵🇹" },
   { code: "uk", label: "UA 🇺🇦" },
 ];
 
-export const DEFAULT_LANGUAGE = LANGUAGES[0].code; // * "en"
+export const DEFAULT_LANGUAGE = LANGUAGES[2].code; // * "en"
 
 export const SUPPORTED_LANGUAGE_CODES = new Set(LANGUAGES.map(({ code }) => code));
 
