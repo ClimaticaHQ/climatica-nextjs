fb01cd5c33c832f3be3a9a65211742de4b3de2d2
Andrew Kononenko <wastardy.k@gmail.com>
2026-09-14 15:32:35 +0200

    feat(hooks): add useDelayedHide hook for controlled visibility with delay

--- src/hooks/ui/index.ts
+++ src/hooks/ui/index.ts
@@ -1,4 +1,5 @@
 export { useDebounce } from "./useDebounce";
+export { useDelayedHide } from "./useDelayedHide";
 export { useGeolocation } from "./useGeolocation";
 export { useHasHydrated } from "./useHasHydrated";
 export { useTheme } from "./useTheme";
